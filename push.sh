a4ce05f2601281b012f58608fd4d40f35aa726b2
/setup.sh <adam>
2018-11-07 11:05:49 -0500

    added a setup file

--- push.sh
+++ push.sh
@@ -2,15 +2,12 @@ if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
     echo "Usage:"
-    echo "'./push.sh YOURNAME MESSAGE'"
+    echo "'./push.sh YOURNAME \"MESSAGE\"'"
     exit 1
 fi
 
 NAME=$1
 MESSAGE=$2
-
-
-# echo $MESSAGE
 
 git reset
 git add .
